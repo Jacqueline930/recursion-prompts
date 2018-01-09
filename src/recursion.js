11babf9503ca994d066ff47607f9f200814c7b13
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-01-08 19:40:04 -0600

    worked to modulo problem

--- src/recursion.js
+++ src/recursion.js
@@ -103,10 +103,21 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string === "") {
+    return "";
+  }
+  return reverse(string.substr(1)) + string.charAt(0);
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  newString = string.replace(/\s/g, '').toLowerCase();
+  if(newString.length === 0 || newString.length === 1) {
+    return true;
+  } else if (newString[0] === newString[newString.length -1]){
+    return palindrome(newString.slice(1, newString.length - 1));
+  }
+  return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -115,6 +126,10 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (x === 0) {
+    return 0;
+  }
+
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
